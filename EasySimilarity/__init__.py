ebda41ac5d7a365ff2457db020652e89a3bc4b6a
xiaorrancs <xiaoranone@gmail.com>
2018-11-17 19:49:01 +0800

    update source

---
+++ EasySimilarity/__init__.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+# Author: xiaoran
+# Time: 2018-11-17 19:21
---
+++ EasySimilarity/simutils.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+# Author: xiaoran
+# Time: 2018-11-17 19:23
+
+class SimilarityUtil(object):
+    '''
+    支持计算两个对应的相似度，参数可以list，tuple，字符串，字典
+    提供的函数，支持自定义函数，
+
+    '''
+    def __init__(self):
+        pass
+
+    def similarity(self):
+        pass
